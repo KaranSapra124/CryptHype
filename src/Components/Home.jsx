42a97851ee3cd68d74a0d2e43b97bd4433d236dc
KaranSapra124 <sapra7112@gmail.com>
2024-06-24 14:08:13 +0530

    Added Material UI

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { ArrowDownwardOutlined } from "@mui/icons-material";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+
+import React from "react";
 
 const Home = () => {
+  const dummyList = ["List 1", "List 2", "List 3", "List 4"];
   return (
-    <div>Home</div>
-  )
-}
+    <Accordion>
+      <AccordionSummary expandIcon={<ArrowDownwardOutlined />}>
+        Crypto 1
+      </AccordionSummary>
+      <AccordionDetails>
+        <ul>
+          {dummyList.map((elem)=>{
+            return <li>{elem}</li>
+          })}
+        </ul>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
-export default Home
+export default Home;
